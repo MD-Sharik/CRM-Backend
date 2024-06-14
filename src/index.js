18e807148ba2b40e0045a12d820a81cda99f0c8e
MD Sharik <md.sharik5012@gmail.com>
2024-06-14 11:47:47 +0530

    asdawd

--- src/index.js
+++ src/index.js
@@ -1,56 +1,3 @@
-// import express from "express";
-// import dbConnect from "./DB/user.db.js";
-// import router1 from "./Router/Authentication.route.js";
-// import { config } from "dotenv";
-// import cors from "cors";
-// import loanRoute from "./Router/loanRoute.route.js";
-
-// const app = express();
-// // Load environment variables
-// config();
-// // Middleware to parse JSON bodies
-// app.use(express.json());
-// // Middleware to parse URL-encoded bodies
-// app.use(
-//   express.urlencoded({
-//     extended: true,
-//     limit: "10mb",
-//     parameterLimit: 1000000,
-//   })
-// );
-
-// // CORS options configuration
-// const corsOptions = {
-//   origin: "https://crm-eosin-six.vercel.app",
-//   optionsSuccessStatus: 200,
-// };
-
-// // Apply CORS middleware globally
-// app.use(cors(corsOptions));
-
-// // Define routes
-// app.use("/api/v1/", router1);
-// app.use("/api/v2/", loanRoute);
-
-// // Default route
-// app.get("/", (req, res) => {
-//   res.status(200).send("Welcome");
-// });
-
-// // Connect to the database and start the server
-// dbConnect()
-//   .then(() => {
-//     const PORT = process.env.PORT || 5000;
-//     app.listen(PORT, () => {
-//       console.log(`Server is running on port ${PORT}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
-// export default app;
-
 import express from "express";
 import dbConnect from "./DB/user.db.js";
 import router1 from "./Router/Authentication.route.js";
@@ -71,10 +18,12 @@ app.use(
   })
 );
 
-// CORS options configuration
+const corsOptions = {
+  origin: "*",
+};
 
 // Apply CORS middleware globally
-app.use(cors());
+app.use(cors(corsOptions));
 
 // Define routes
 app.use("/api/v1/", router1);
